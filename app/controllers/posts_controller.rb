188ec6e606e4cf6fa754e24dfb66335d27adfc1c
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-17 20:04:07 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update!(post_params)
+    @post.update(post_params)
     if @post.valid?
       @post.save
       redirect_to post_path(@post)
